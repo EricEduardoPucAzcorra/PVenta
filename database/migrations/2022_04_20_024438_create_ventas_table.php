24b20773b99050cbd94e2622ab7f5761bb796367
Eric <azcorraeduardo43@gmail.com>
2022-08-13 15:47:52 -0500

    Curso udemy

---
+++ database/migrations/2022_04_20_024438_create_ventas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->increments('id_venta');
+            $table->integer('id_cliente')->unsigned();
+            $table->foreign('id_cliente')->references('id_cliente')->on('personas');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
