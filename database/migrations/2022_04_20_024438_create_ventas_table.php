bd143e9da05bbaaa4aa38125fc1f280d1d04e202
Eric Puc Azcorra <72041561+EricEduardoPucAzcorra@users.noreply.github.com>
2022-08-13 21:26:22 -0500

    Ventas migrations

--- database/migrations/2022_04_20_024438_create_ventas_table.php
+++ database/migrations/2022_04_20_024438_create_ventas_table.php
@@ -16,8 +16,16 @@ class CreateVentasTable extends Migration
         Schema::create('ventas', function (Blueprint $table) {
             $table->increments('id_venta');
             $table->integer('id_cliente')->unsigned();
-            $table->foreign('id_cliente')->references('id_cliente')->on('personas');
-            
+            $table->foreign('id_cliente')->references('id_persona')->on('personas'); 
+            $table->integer('id_usuario')->unsigned();
+            $table->foreign('id_usuario')->references('id_usuario')->on('users');
+            $table->string('tipo_documento',20);
+            $table->string('serie_comprobante',7)->nullable();
+            $table->string('num_documento',10);
+            $table->dateTime('fecha_hora');
+            $table->float('impuesto',11,2);
+            $table->float('total',11,2);
+            $table->string('estado');
             $table->timestamps();
         });
     }
--- database/migrations/2022_04_20_024805_create_detalle_ventas_table.php
+++ database/migrations/2022_04_20_024805_create_detalle_ventas_table.php
@@ -14,8 +14,15 @@ class CreateDetalleVentasTable extends Migration
     public function up()
     {
         Schema::create('detalle_ventas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->integer('id_venta')->unsigned();
+            $table->foreign('id_venta')->references('id_venta')->on('ventas')->onDelete('cascade');
+            $table->integer('id_articulo')->unsigned();
+            $table->foreign('id_articulo')->references('id_articulo')->on('articulos'); 
+            $table->integer('cantidad');
+            $table->float('precio',11,2);
+            $table->float('descuento',11,2);
+            // $table->timestamps();
         });
     }
 
